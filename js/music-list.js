d55f8ead78eaf23b02c6a94d56e1603bbd65ffbd
ahmed sameh <as6864886@gmail.com>
2023-06-24 02:41:21 +0200

    player

---
+++ js/music-list.js
@@ -0,0 +1,694 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music-music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "سوره الفاتحه",
+    artist: "الشيخ مشاري راشد",
+    src: "music-1"
+  },
+  {
+    name: "سوره البقره",
+    artist: "الشيخ مشاري راشد",
+    src: "music-2"
+  },
+  {
+    name: "سورة آل عمران",
+    artist: "الشيخ مشاري راشد",
+    src: "music-3"
+  },
+  {
+    name: "سوره النساء",
+    artist: "الشيخ مشاري راشد",
+    src: "music-4"
+  },
+  {
+    name: "سوره المائده",
+    artist: "الشيخ مشاري راشد",
+    src: "music-5"
+  },
+  {
+    name: "سوره الانعام",
+    artist: "الشيخ مشاري راشد",
+    src: "music-6"
+  },
+  {
+    name:"سورة الأعراف",
+    artist: "الشيخ مشاري راشد",
+    src: "music-7"
+  },
+  {
+    name: "سورة الأنفال",
+    artist: "الشيخ مشاري راشد",
+    src: "music-8"
+  },
+  {
+    name: "سورة التوبة",
+    artist: "الشيخ مشاري راشد",
+    src: "music-9"
+  },
+  {
+    name: "سورة يونس",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-10"
+  },
+  {
+    name: "سورة هود",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-11"
+  },
+  {
+    name: "سورة يوسف",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-12"
+  },
+  {
+    name: "سورة الرعد",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-13"
+  },
+  {
+    name: "سورة إبراهيم",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-14"
+  },
+  {
+    name: "سورة الحجر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-15"
+  },
+  {
+    name: "سورة النحل",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-16"
+  },
+  {
+    name: "سورة الإسراء",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-17"
+  },
+  {
+    name: "سورة الكهف",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-18"
+  },
+  {
+    name: "سورة مريم",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-19"
+  },
+  {
+    name: "سورة طه",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-20"
+  },
+  {
+    name: "سورة الأنبياء",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-21"
+  },
+  {
+    name: "سورة الحج",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-22"
+  },
+  {
+    name: "سورة المؤمنون",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-023"
+  },
+  {
+    name: "سورة النّور",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-24"
+  },
+  {
+    name: "سورة الفرقان",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-25"
+  },
+  {
+    name: "سورة الشعراء",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-26"
+  },
+  {
+    name: "سورة النّمل",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-27"
+  },
+  {
+    name: "سورة القصص",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-28"
+  },
+  {
+    name: "سورة العنكبوت",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-29"
+  },
+  {
+    name: "سورة الرّوم",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-30"
+  },
+  {
+    name: "سورة لقمان",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-31"
+  },
+  {
+    name: "سورة السجدة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-32"
+  },
+  {
+    name: "سورة الأحزاب",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-33"
+  },
+  {
+    name: "سورة سبأ",
+    artist: "الشيخ مشاري راشد",
+    src: "music-34"
+  },
+  {
+    name: "سورة فاطر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-35"
+  },
+  {
+    name: "سورة يس",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-36"
+  },
+  {
+    name: "سورة الصافات",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-37"
+  },
+  {
+    name: "سورة ص",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-38"
+  },
+  {
+    name: "سورة الزمر",
+    artist: "الشيخ مشاري راشد",
+    src: "music-39"
+  },
+  {
+    name: "سورة غافر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-40"
+  },
+  {
+    name: "سورة فصّلت",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-41"
+  },
+  {
+    name: "سورة الشورى",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-42"
+  },
+  {
+    name: "سورة الزخرف",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-43"
+  },
+  {
+    name: "سورة الدّخان",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-44"
+  },
+  {
+    name: "سورة الجاثية",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-45"
+  },
+  {
+    name: "سورة الأحقاف",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-46"
+  },
+  {
+    name: "سورة محمد",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-47"
+  },
+  {
+    name: "سورة الفتح",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-48"
+  },
+  {
+    name: "سورة الحجرات",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-49"
+  },
+  {
+    name: "سورة ق",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-50"
+  },
+  {
+    name: "سورة الذاريات",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-51"
+  },
+  {
+    name: "سورة الطور",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-52"
+  },
+  {
+    name: "سورة النجم",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-53"
+  },
+  {
+    name: "سورة القمر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-54"
+  },
+  {
+    name: "سورة الرحمن",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-55"
+  },
+  {
+    name: "سورة الواقعة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-56"
+  },
+  {
+    name: "سورة الحديد",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-57"
+  },
+  {
+    name: "سورة المجادلة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-58"
+  },
+  {
+    name: "سورة الحشر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-59"
+  },
+  {
+    name: "سورة الممتحنة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-60"
+  },
+  {
+    name: "سورة الصف",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-61"
+  },
+  {
+    name: "سورة الجمعة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-62"
+  },
+  {
+    name: "سورة المنافقون",
+    artist: "الشيخ مشاري راشد",
+    src: "music-63"
+  },
+  {
+    name: "سورة التغابن",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-64"
+  },
+  {
+    name: "سورة الطلاق",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-65"
+  },
+  {
+    name: "سورة التحريم",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-66"
+  },
+  {
+    name: "سورة الملك",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-67"
+  },
+  {
+    name: "سورة القلم",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-68"
+  },
+  {
+    name: "سورة الحاقة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-69"
+  },
+  {
+    name: "سورة المعارج",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-70"
+  },
+  {
+    name: "سورة نوح",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-71"
+  },
+  {
+    name: "سورة الجن",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-72"
+  },
+  {
+    name: "سورة المزّمّل",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-73"
+  },
+  {
+    name: "سورة المدّثر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-74"
+  },
+  {
+    name: "سورة القيامة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-75"
+  },
+  {
+    name: "سورة الإنسان",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-76"
+  },
+  {
+    name: "سورة المرسلات",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-77"
+  },
+  {
+    name: "سورة النبأ",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-78"
+  },
+  {
+    name: "سورة النازعات",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-79"
+  },
+  {
+    name: "سورة عبس",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-80"
+  },
+  {
+    name: "سورة التكوير",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-81"
+  },
+  {
+    name: "سورة الانفطار",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-82"
+  },
+  {
+    name: "سورة المطفّفين",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-83"
+  },
+  {
+    name: "سورة الانشقاق",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-84"
+  },
+  {
+    name: "سورة البروج",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-85"
+  },
+  {
+    name: "سورة الطارق",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-86"
+  },
+  {
+    name: "سورة الأعلى",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-87"
+  },
+  {
+    name: "سورة الغاشية",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-88"
+  },
+  {
+    name: "سورة الفجر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-89"
+  },
+  {
+    name: "سورة البلد",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-90"
+  },
+  {
+    name: "سورة الشمس",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-91"
+  },
+  {
+    name: "سورة الليل",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-92"
+  },
+  {
+    name: "سورة الضحى",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-93"
+  },
+  {
+    name: "سورة الشرح",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-94"
+  },
+  {
+    name: "سورة التين",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-95"
+  },
+  {
+    name: "سورة العلق",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-96"
+  },
+  {
+    name: "سورة القدر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-97"
+  },
+  {
+    name: "سورة البينة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-98"
+  },
+  {
+    name: "سورة الزلزلة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-99"
+  },
+  {
+    name: "سورة العاديات",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-100"
+  },
+  {
+    name: "سورة القارعة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-101"
+  },
+  {
+    name: "سورة التكاثر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-102"
+  },
+  {
+    name: "سورة العصر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-103"
+  },
+  {
+    name: "سورة الهمزة",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-104"
+  },
+  {
+    name: "سورة الفيل",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-105"
+  },
+  {
+    name: "سورة قريش",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-106"
+  },
+  {
+    name: "سورة الماعون",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-107"
+  },
+  {
+    name: "سورة الكوثر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-108"
+  },
+  {
+    name: "سورة الكافرون",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-109"
+  },
+  {
+    name: "سورة النصر",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-110"
+  },
+  {
+    name: "سورة المسد",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-111"
+  },
+  {
+    name: "سورة الإخلاص",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-112"
+  },
+  {
+    name: "سورة الفلق",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-113"
+  },
+  {
+    name: "سورة النّاس",
+    artist: "الشيخ مشاري راشد",
+    
+    src: "music-114"
+  },
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music-music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
